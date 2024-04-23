11dda504465234607a2c3ffd27d601376ede6a8e
john <dontreplypls@google.com>
2024-04-22 18:31:45 -0700

    Added a new file to add events, and add them to the calendar on the correct day

--- calendar/cal.py
+++ calendar/cal.py
@@ -1,29 +1,36 @@
 import calendar
 import datetime
 import time
-
+# WARNING: Do not edit calendar.txt
 years_out = 5
 int_to_str_months = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August",
                      9: "September", 10: "October", 11: "November", 12: "December"}
 date = str(datetime.date.today()).split("-")
 year = int(date[0])
 month = int(date[1])
-print("month is " + str(month))
-cal_file = open("calendar.txt", "a+")
-cal_file.raw_str = cal_file.read()
-date_not_split = str(datetime.date.today())
-for i in range(month, 13):
-    if i > 12:
-        break
-    print(i)
-    cal_file.write(f"{int_to_str_months[i]} {year} {calendar.monthrange(year, i)[1]} days \n")
-    for x in range(calendar.monthrange(year, i)[1]):
-        cal_file.write(f"{str(x + 1)} \n")
-for y in range(years_out - 1):
-    for i in range(1, 13):
+cal_file_check = open("calendar.txt", "r")
+cal_file_check.lines = cal_file_check.readlines()
+if f"April {str(year + years_out - 2)}\n" in cal_file_check.lines:
+    print("file already created, keeping original")
+else:
+    print("creating and writing to file...")
+    cal_file = open("calendar.txt", "a+")
+    cal_file.raw_str = cal_file.read()
+    print(f"the file is {cal_file.raw_str} end of file")
+    date_not_split = str(datetime.date.today())
+    for i in range(month, 13):
         if i > 12:
             break
-        print(i)
-        cal_file.write(f"{int_to_str_months[i]} {year + y} {calendar.monthrange(year + y, i)[1]} days \n")
-        for x in range(calendar.monthrange(year + y, i)[1]):
+        cal_file.write(f"{int_to_str_months[i]} {year}\n")
+        for x in range(calendar.monthrange(year, i)[1]):
             cal_file.write(f"{str(x + 1)} \n")
+    for y in range(1, years_out - 1):
+        for i in range(1, 13):
+            if i > 12:
+                break
+            cal_file.write(f"{int_to_str_months[i]} {year + y}\n")
+            for x in range(calendar.monthrange(year + y, i)[1]):
+                cal_file.write(f"{str(x + 1)} \n")
+    print("file finished")
+    cal_file.close()
+
---
+++ calendar/make_new_event.py
@@ -0,0 +1,26 @@
+#  date format mm/dd/yyyy
+int_to_str_months = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August",
+                     9: "September", 10: "October", 11: "November", 12: "December"}
+
+
+def make_new_event(title: str, description: str, date):
+    event_file = open("events.txt", "a")
+    event_file.write(f"({date}) ({title}) ({description})\n")
+    event_file.close()
+    event_file = open("events.txt", "r")
+    index = len(event_file.readlines()) - 1
+    event_file.close()
+    calendar_file = open("calendar.txt", "r+")
+    cal_file_lines = calendar_file.readlines()
+    date_point = f"{int_to_str_months[date[0]]} {date[2]}\n"
+    print(f"the line you want to put the date is {cal_file_lines.index(date_point) + date[0] +1}")
+
+    cal_file_lines[cal_file_lines.index(date_point) + date[0]] = f"{cal_file_lines[cal_file_lines.index(date_point)+date[0]][:-2]} {index}\n"
+    calendar_file.close()
+    calendar_file = open("calendar.txt", "w")
+    calendar_file.writelines(cal_file_lines)
+    calendar_file.close()
+    print(index)
+
+
+make_new_event("testing", "I am testing if this works", [4, 1, 2025])
