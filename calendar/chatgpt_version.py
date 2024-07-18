0505be1a8fa408374eae229a60752c3d2d5227d7
john <dontreplypls@google.com>
2024-07-18 12:25:02 -0700

    Mostly finished using chatgpt to read events from gcal. No audio functionality though, since I'm trying to use this for the server. I still need to fine tune the prompts and settings to minimize tokens and provide better outputs.

--- calendar/chatgpt_version.py
+++ calendar/chatgpt_version.py
@@ -9,6 +9,35 @@ import os
 with open("openai_tok.json", "r") as token:
     api_tok = json.load(token)["Token"]
 
+tools = [
+  {
+    "type": "function",
+    "function": {
+      "name": "get_current_events",
+      "description": "Gets the calendar events for the specified day",
+      "parameters": {
+        "type": "object",
+        "properties": {
+          "day": {
+            "type": "integer",
+            "description": "The day of the event",
+          },
+          "month": {
+                "type": "integer",
+                "description": "The month of the event",
+          },
+          "year": {
+                "type": "integer",
+                "description": "The year of the event",
+          },
+    #"required": ["day", "month", "year"]
+        }
+      }
+    }
+  }
+]
+
+
 # refresh token.json if scopes get changed
 SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 
@@ -31,6 +60,45 @@ if not creds or not creds.valid:
     # Save the credentials for the next run
     with open("token.json", "w") as token:
         token.write(creds.to_json())
+
+
+
+def find_gcal_events_day(day:int, month: int, year: int):
+
+    try:
+        service = build("calendar", "v3", credentials=creds)
+        # Call the Calendar API
+        print("Getting the events for today")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=f'{year}-{month}-{day}T00:00:00-07:00',
+                timeMax=f'{year}-{month}-{day}T23:59:00-07:00',
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return "No events were found"
+
+        # Prints the start and name of the next 10 events
+        return_str = f"There are {len(events)} events for that day: "
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+            return_str += f"{event['summary']} - {event['description']} at {start}, "
+        return return_str
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
+
+
 url = "https://jamsapi.hackclub.dev/openai"
 
 client = OpenAI(
@@ -42,10 +110,29 @@ chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "Say this is a test",
+            "content": "Show me the events for the day April 1 2025",
+        }
+    ],
+    model="gpt-3.5-turbo",
+    tools=tools,
+    tool_choice="auto"
+)
+print(chat_completion.choices[0].message.tool_calls[0].function.arguments)
+func_resp = chat_completion.choices[0].message.tool_calls[0].function
+json_rsp = json.loads(func_resp.arguments)
+if func_resp.name == "get_current_events":
+    gcal_resp = find_gcal_events_day(json_rsp["day"], json_rsp["month"], json_rsp["year"])
+else:
+    gcal_resp = "There was an error. Try rephrasing"
+print(gcal_resp)
+
+chat_completion2 = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": f"Summarize the output of a function for the user: {gcal_resp}",
         }
     ],
     model="gpt-3.5-turbo",
 )
-#response = json.load(chat_completion)
-print(chat_completion.choices[0].message.content)
+print(chat_completion2.choices[0].message.content)
